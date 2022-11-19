7c0324113cbb92bf9ac386b9dd434dd84c3f975f
hadashiA <dev@hadashikick.jp>
2022-11-19 15:51:08 +0900

    sample proj

---
+++ SampleVYaml/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using VYaml;
+
+var path = Path.Combine(Directory.GetCurrentDirectory(), "sample_envoy.yaml");
+var bytes = File.ReadAllBytes(path);
+var parser = Parser.FromBytes(bytes);
+
+while (parser.Read())
+{
+}
+
