3622dc72d2084522fd06a8210f12078d94a7660b
hadashiA <dev@hadashikick.jp>
2025-05-13 11:38:37 +0900

    Improve ascii code utils

--- VYaml/Internal/YamlCodes.cs
+++ VYaml/Internal/YamlCodes.cs
@@ -6,67 +6,63 @@ namespace VYaml.Internal
 {
     public static class YamlCodes
     {
-        public static readonly byte[] YamlDirectiveName = { (byte)'Y', (byte)'A', (byte)'M', (byte)'L' };
-        public static readonly byte[] TagDirectiveName = { (byte)'T', (byte)'A', (byte)'G' };
+        public static readonly byte[] YamlDirectiveName = "YAML"u8.ToArray();
+        public static readonly byte[] TagDirectiveName = "TAG"u8.ToArray();
 
-        public static readonly byte[] Utf8Bom = { 0xEF, 0xBB, 0xBF };
-        public static readonly byte[] StreamStart = { (byte)'-', (byte)'-', (byte)'-' };
-        public static readonly byte[] DocStart = { (byte)'.', (byte)'.', (byte)'.' };
-        public static readonly byte[] CrLf = { Cr, Lf };
+        public static readonly byte[] Utf8Bom = [0xEF, 0xBB, 0xBF];
+        public static readonly byte[] StreamStart = "---"u8.ToArray();
+        public static readonly byte[] DocStart = "..."u8.ToArray();
+        public static readonly byte[] CrLf = "\r\n"u8.ToArray();
 
-        public static readonly byte[] Null0 = { (byte)'n', (byte)'u', (byte)'l', (byte)'l' };
-        public static readonly byte[] Null1 = { (byte)'N', (byte)'u', (byte)'l', (byte)'l' };
-        public static readonly byte[] Null2 = { (byte)'N', (byte)'U', (byte)'L', (byte)'L' };
+        public static readonly byte[] Null0 = "null"u8.ToArray();
+        public static readonly byte[] Null1 = "Null"u8.ToArray();
+        public static readonly byte[] Null2 = "NULL"u8.ToArray();
         public const byte NullAlias = (byte)'~';
 
-        public static readonly byte[] True0 = { (byte)'t', (byte)'r', (byte)'u', (byte)'e' };
-        public static readonly byte[] True1 = { (byte)'T', (byte)'r', (byte)'u', (byte)'e' };
-        public static readonly byte[] True2 = { (byte)'T', (byte)'R', (byte)'U', (byte)'E' };
+        public static readonly byte[] True0 = "true"u8.ToArray();
+        public static readonly byte[] True1 = "True"u8.ToArray();
+        public static readonly byte[] True2 = "TRUE"u8.ToArray();
 
-        public static readonly byte[] False0 = { (byte)'f', (byte)'a', (byte)'l', (byte)'s', (byte)'e' };
-        public static readonly byte[] False1 = { (byte)'F', (byte)'a', (byte)'l', (byte)'s', (byte)'e' };
-        public static readonly byte[] False2 = { (byte)'F', (byte)'A', (byte)'L', (byte)'S', (byte)'E' };
+        public static readonly byte[] False0 = "false"u8.ToArray();
+        public static readonly byte[] False1 = "False"u8.ToArray();
+        public static readonly byte[] False2 = "FALSE"u8.ToArray();
 
-        public static readonly byte[] Inf0 = { (byte)'.', (byte)'i', (byte)'n', (byte)'f' };
-        public static readonly byte[] Inf1 = { (byte)'.', (byte)'I', (byte)'n', (byte)'f' };
-        public static readonly byte[] Inf2 = { (byte)'.', (byte)'I', (byte)'N', (byte)'F' };
-        public static readonly byte[] Inf3 = { (byte)'+', (byte)'.', (byte)'i', (byte)'n', (byte)'f' };
-        public static readonly byte[] Inf4 = { (byte)'+', (byte)'.', (byte)'I', (byte)'n', (byte)'f' };
-        public static readonly byte[] Inf5 = { (byte)'+', (byte)'.', (byte)'I', (byte)'N', (byte)'F' };
+        public static readonly byte[] Inf0 = ".inf"u8.ToArray();
+        public static readonly byte[] Inf1 = ".Inf"u8.ToArray();
+        public static readonly byte[] Inf2 = ".INF"u8.ToArray();
+        public static readonly byte[] Inf3 = "+.inf"u8.ToArray();
+        public static readonly byte[] Inf4 = "+.Inf"u8.ToArray();
+        public static readonly byte[] Inf5 = "+.INF"u8.ToArray();
 
-        public static readonly byte[] Yes0 = { (byte)'y', (byte)'e', (byte)'s' };
-        public static readonly byte[] Yes1 = { (byte)'Y', (byte)'e', (byte)'s' };
-        public static readonly byte[] Yes2 = { (byte)'Y', (byte)'E', (byte)'S' };
+        public static readonly byte[] Yes0 = "yes"u8.ToArray();
+        public static readonly byte[] Yes1 = "Yes"u8.ToArray();
+        public static readonly byte[] Yes2 = "YES"u8.ToArray();
 
-        public static readonly byte[] No0 = { (byte)'n', (byte)'o' };
-        public static readonly byte[] No1 = { (byte)'N', (byte)'o' };
-        public static readonly byte[] No2 = { (byte)'N', (byte)'O' };
+        public static readonly byte[] No0 = "no"u8.ToArray();
+        public static readonly byte[] No1 = "No"u8.ToArray();
+        public static readonly byte[] No2 = "NO"u8.ToArray();
 
-        public static readonly byte[] On0 = { (byte)'o', (byte)'n' };
-        public static readonly byte[] On1 = { (byte)'O', (byte)'n' };
-        public static readonly byte[] On2 = { (byte)'O', (byte)'N' };
+        public static readonly byte[] On0 = "on"u8.ToArray();
+        public static readonly byte[] On1 = "On"u8.ToArray();
+        public static readonly byte[] On2 = "ON"u8.ToArray();
 
-        public static readonly byte[] Off0 = { (byte)'o', (byte)'f', (byte)'f' };
-        public static readonly byte[] Off1 = { (byte)'O', (byte)'f', (byte)'f' };
-        public static readonly byte[] Off2 = { (byte)'O', (byte)'F', (byte)'F' };
+        public static readonly byte[] Off0 = "off"u8.ToArray();
+        public static readonly byte[] Off1 = "Off"u8.ToArray();
+        public static readonly byte[] Off2 = "OFF"u8.ToArray();
 
-        public static readonly byte[] NegInf0 = { (byte)'-', (byte)'.', (byte)'i', (byte)'n', (byte)'f' };
-        public static readonly byte[] NegInf1 = { (byte)'-', (byte)'.', (byte)'I', (byte)'n', (byte)'f' };
-        public static readonly byte[] NegInf2 = { (byte)'-', (byte)'.', (byte)'I', (byte)'N', (byte)'F' };
+        public static readonly byte[] NegInf0 = "-.inf"u8.ToArray();
+        public static readonly byte[] NegInf1 = "-.Inf"u8.ToArray();
+        public static readonly byte[] NegInf2 = "-.INF"u8.ToArray();
 
-        public static readonly byte[] Nan0 = { (byte)'.', (byte)'n', (byte)'a', (byte)'n' };
-        public static readonly byte[] Nan1 = { (byte)'.', (byte)'N', (byte)'a', (byte)'N' };
-        public static readonly byte[] Nan2 = { (byte)'.', (byte)'N', (byte)'A', (byte)'N' };
+        public static readonly byte[] Nan0 = ".nan"u8.ToArray();
+        public static readonly byte[] Nan1 = ".NaN"u8.ToArray();
+        public static readonly byte[] Nan2 = ".NAN"u8.ToArray();
 
-        public static readonly byte[] HexPrefix = { (byte)'0', (byte)'x' };
-        public static readonly byte[] HexPrefixNegative = { (byte)'-', (byte)'0', (byte)'x' };
+        public static readonly byte[] HexPrefix = "0x"u8.ToArray();
+        public static readonly byte[] HexPrefixNegative = "-0x"u8.ToArray();
 
-        public static readonly byte[] OctalPrefix = { (byte)'0', (byte)'o' };
-
-        public static readonly byte[] UnityStrippedSymbol =
-        {
-            (byte)'s', (byte)'t', (byte)'r', (byte)'i', (byte)'p', (byte)'p', (byte)'e', (byte)'d'
-        };
+        public static readonly byte[] OctalPrefix = "0o"u8.ToArray();
+        public static readonly byte[] UnityStrippedSymbol = "stripped"u8.ToArray();
 
         public const byte Space = (byte)' ';
         public const byte Tab = (byte)'\t';
@@ -169,7 +165,7 @@ namespace VYaml.Internal
         public static bool IsAscii(byte code) => code <= '\x7F';
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsNumber(byte code) => code is >= (byte)'0' and <= (byte)'9';
+        public static bool IsNumber(byte c) => (byte)((c | 0x20) - (byte)'0') < 10;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsEmpty(byte code) => code is Space or Tab or Lf or Cr;
@@ -181,15 +177,10 @@ namespace VYaml.Internal
         public static bool IsBlank(byte code) => code is Space or Tab;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsNumberRepresentation(byte code) => code is
-            >= (byte)'0' and <= (byte)'9' or
-            (byte)'+' or (byte)'-' or (byte)'.';
+        public static bool IsHexAlphabet(byte c) => (byte)((c | 0x20) - (byte)'a') < 6;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsHex(byte code) => code is
-            >= (byte)'0' and <= (byte)'9' or
-            >= (byte)'A' and <= (byte)'F' or
-            >= (byte)'a' and <= (byte)'f';
+        public static bool IsHex(byte code) => IsNumber(code) || IsHexAlphabet(code);
 
         // Spec: https://yaml.org/spec/1.2.2/#rule-c-flow-indicator
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -197,12 +188,19 @@ namespace VYaml.Internal
             (byte)',' or (byte)'[' or (byte)']' or (byte)'{' or (byte)'}';
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static byte AsHex(byte code) => code switch
+        public static byte AsHex(byte code)
         {
-            >= (byte)'0' and <= (byte)'9' => (byte)(code - (byte)'0'),
-            >= (byte)'a' and <= (byte)'f' => (byte)(code - (byte)'a' + 10),
-            >= (byte)'A' and <= (byte)'F' => (byte)(code - (byte)'A' + 10),
-            _ => throw new InvalidOperationException()
-        };
+            var x = code - (byte)'0';
+            if ((uint)x <= 9)
+            {
+                return (byte)x;
+            }
+            x = (code | 0x20) - (byte)'a';
+            if ((uint)x <= 5)
+            {
+                return (byte)(x + 10);
+            }
+            throw new InvalidOperationException();
+        }
     }
 }
